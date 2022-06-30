fd79806fa3ad79127acd1e189ccba8b251c34b84
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2022-06-30 16:02:50 -0700

    Create CERT-Prompt-Nmap-ScanSubnet.ps1
    certs on subnet

---
+++ CERT-Prompt-Nmap-ScanSubnet.ps1
@@ -0,0 +1,19 @@
+## Discover Certificates on Subnet
+##### Nmap SSL-Cert -- scan a selected subnet for bound certitficates and report in all three nmap formats
+##### author: Kristopher F. Haughey
+
+$TempDir = Read-Host -Prompt "Enter the path to a temp directory"
+$TempFile = "CertScan"
+$TargetList = Read-Host -Prompt "Enter the full path and name of the target list (e.g. - c:\Temp\TargetList.csv)" | Import-csv
+foreach ($Target in $TargetList){
+	$subnet = $target.subnet
+	$nickName = $target.nickName
+    	Invoke-Command -ScriptBlock {nmap $subnet --script ssl-cert -sV -sC -oA $TempDir$nickName$TempFile}
+}
+#### sample CSV
+'@subnet,nickName
+192.168.251.0/24,251
+192.168.200.0/24,200
+192.168.103.0/24,103
+192.168.194.0/24,194
+@'
